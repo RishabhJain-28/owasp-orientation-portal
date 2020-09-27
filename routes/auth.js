500c56b2497873cdf66ec224128097d10a79251e
Rishabh Jain <rjain_be19@thapar.edu>
2020-09-28 03:37:37 +0530

    added https on redirects

--- routes/auth.js
+++ routes/auth.js
@@ -47,13 +47,17 @@ router.get("/login/callback", (req, res, next) => {
     function (err, user, info) {
       // console.log(err, user, info);
       if (!user)
-        return res.redirect(`${process.env.CLIENT_URL}/error?msg=${err}`);
+        return res.redirect(
+          `https://owasp-orientation.herokuapp.com/error?msg=${err}`
+        );
       req.logIn(user, function (err) {
         if (err) {
           return next(err);
         }
 
-        return res.redirect(`${process.env.CLIENT_URL}/dashboard`);
+        return res.redirect(
+          `https://owasp-orientation.herokuapp.com/dashboard`
+        );
       });
     }
   )(req, res, next);
