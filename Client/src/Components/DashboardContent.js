caccb0f4a1e8661bf28d432abc20beb20f9daa7c
Rishabh Jain <rjain_be19@thapar.edu>
2020-10-01 04:56:47 +0530

    recruitment content done (#11)
    Co-authored-by: Sushila Kaur <56120047+sushilakaur@users.noreply.github.com>

--- Client/src/Components/DashboardContent.js
+++ Client/src/Components/DashboardContent.js
@@ -27,13 +27,20 @@ const DashboardContent = ({ quizzes, name }) => {
             <div className="jumbotron">
               <p>
                 Hey {name},Thank you for registering! We welcome you to the
-                Owasp student chapter portal! Recruitments will commence soon .
-                Stay tuned!
+                Owasp student chapter portal! <br></br>
+                <strong>In order to be irreplaceable, one must always be different.</strong><br></br>
+                The world today requires multi-tasking and multi-skilled engineers who can write the most efficient code at one time and pitch the 
+                business ideas on the other. OWASP Student Chapter TIET, an elite coding club, that prepares you for the Tech world and simultaneously 
+                equips you with Non-Tech skills, is officially going to start the recruitments for First Year and Second Year from OCTOBER 1, 2020.
+                <br></br>
+                For 1st year candidates : On 1st October, quiz will be held in two slots i.e at <strong>5:30pm</strong> and <strong>7:30pm</strong>. You can choose any <strong>one</strong> slot.
+                <br></br>
+                For 2nd year candidates : On 1st October, Recruitment form will be live at <strong>5:30pm</strong>.
               </p>
             </div>
             <div className="row">
               <div className="flex-container">
-                <div className="flex-item">
+                {/* <div className="flex-item">
                   <div className="flex-item-inner">
                     <div className="card-front">
                       <i
@@ -42,17 +49,17 @@ const DashboardContent = ({ quizzes, name }) => {
 
                       <h3>Recruitment link</h3>
                     </div>
-                    {/* <a href={"http://bit.ly/OWASPorientation"}> */}
+                    <a href={"http://bit.ly/OWASPorientation"}>
                     <div className="card-back bg-dark">
-                      {/* <p className="title">{title}</p> */}
+                      <p className="title">{title}</p>
                       <p className="desc">Will be live soon.</p>
                       <p className="link">
                         <i className="fa fa-chevron-circle-right"></i>
                       </p>
                     </div>
-                    {/* </a> */}
-                  </div>
-                </div>
+                    </a>
+                  </div> */}
+                {/* </div> */}
                 {/* <DashboardCard
                   iconClass="fa fa-video-camera"
                   heading="Orientation link"
@@ -62,16 +69,16 @@ const DashboardContent = ({ quizzes, name }) => {
                 /> */}
                 <DashboardCard
                   iconClass="fa fa-indent"
-                  heading="Fun Quiz link"
-                  title="Fun Quiz"
-                  desc="Results will be out soon"
+                  heading="Recruitment quiz for first year"
+                  title="Only for first year students"
+                  desc="Slot 1 : Quiz will be live at 5:30pm"
                   link={getLink("funquiz")}
                 />
                 <DashboardCard
                   iconClass="fa fa-file-text"
-                  heading="Coding quiz link"
-                  title="Coding quiz"
-                  desc="Results will be out soon"
+                  heading="Recruitment form for 2nd year"
+                  title="Only for Second year students"
+                  desc="Form link will be live at 5:30pm"
                   link={getLink("codingquiz")}
                 />
               </div>
